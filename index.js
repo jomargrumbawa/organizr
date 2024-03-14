58d2ba3b1754be0986b26caa017cfa33884420cc
Jomar Rumbawa <jomargrumbawa@gmail.com>
2024-03-14 10:53:20 -0700

    Add localStorage

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const todosList = document.getElementById("todos")
 const submitBtn = document.getElementById("submit-btn")
 const todoInput = document.getElementById("todo-input")
+let todos = []
+
+if (localStorage.getItem("todos")) {
+  todos = JSON.parse(localStorage.getItem("todos"))
+  todos.forEach(function(todo) {
+    appendToList(todo)
+  })
+}
 
 submitBtn.addEventListener("click", function() {
   const todo = todoInput.value
+  todos.push(todo)
   clearTodoInput()
+  saveToLocalStorage(todos)
   appendToList(todo)
 })
+
+function saveToLocalStorage(list) {
+  localStorage.setItem("todos", JSON.stringify(list))
+}
 
 function appendToList(value) {
   const listItem = document.createElement("li")
